feda2d0eae41cfdfdc731809a1d397722878889b
xed <xed@bebebe.org>
2014-07-25 22:19:46 +0400

    Верстка, поправлен жс

--- wp-services-calculator/plugin.php
+++ wp-services-calculator/plugin.php
@@ -24,69 +24,81 @@ class Plugin
         $sections = Section::getAll();
 
         ?>
-            <script src="/wp-content/plugins/wp-services-calculator/static/calc.js"></script>
+        <script src="/wp-content/plugins/wp-services-calculator/static/calc.js"></script>
 
-            <div id="WSC_resultInfo">
-                Итого: <span id="WSC_sum">0</span> р.
-                <a href="#">Оформить заказ</a>
+        <div id="WSC_resultInfo">
+            Итого: <span id="WSC_sum">0</span> р.
+            <a href="#">Оформить заказ</a>
+        </div>
+
+        <div id="WSC_container">
+
+            <div id="WSC_sections">
+                <?php foreach ($sections as $section): ?>
+                    <div class="WSC_section" data-id="<?php echo $section['id'] ?>">
+                        <?php echo $section['name']; ?>
+                    </div>
+                <?php endforeach; ?>
             </div>
 
-            <div id="WSC_container">
+            <div id="WSC_sectionsContent">
 
-                <div id="WSC_sections">
-                    <?php foreach($sections as $section): ?>
-                        <div class="WSC_section" data-id="<?php echo $section['id'] ?>">
-                            <?php echo $section['name']; ?>
-                        </div>
-                    <?php endforeach; ?>
+                <div id="WSC_sectionsContentPlaceholder" class="WSC_sectionContent">
+                    Выберите категорию услуг слева.
                 </div>
 
-                <div id="WSC_sectionsContent">
+                <?php foreach ($sections as $section): ?>
+                    <div id="WSC_sectionContent_<?php echo $section['id']; ?>" class="WSC_sectionContent hidden">
 
-                    <div id="WSC_sectionsContentPlaceholder" class="WSC_sectionContent">
-                        Тыкайте по ссалкам слева чтобы найти н жопу приключений
-                    </div>
+                        <?php foreach (Category::getBySection($section['id']) as $category): ?>
+                            <div class="WSC_sectionCategory">
 
-                    <?php foreach($sections as $section): ?>
-                        <div id="WSC_sectionContent_<?php echo $section['id']; ?>" class="WSC_sectionContent hidden">
+                                <div class="WSC_sectionCategoryTitle">
+                                    <span><?php echo $category['name']; ?></span>
 
-                            <?php foreach(Category::getBySection($section['id']) as $category): ?>
-                                <div class="WSC_sectionCategory">
+                                    <p><?php echo $category['description']; ?></p>
+                                </div>
 
-                                    <div class="WSC_sectionCategoryTitle">
-                                        <p><?php echo $category['name']; ?></p>
-                                        <p><?php echo $category['description']; ?></p>
-                                    </div>
-
-                                    <div class="WSC_sectionCategoryServices">
-                                        <?php foreach(Service::getByCategory($category['id']) as $service): ?>
-                                            <div class="WSC_service">
-
-                                                <input type="hidden" class="WSC_serviceCostValue" value="<?php echo $service['cost']; ?>" />
-                                                <input type="hidden" class="WSC_serviceManyValue" value="<?php echo $service['many']; ?>" />
-
-                                                <input type="checkbox" class="WSC_serviceCheck" />
-
-                                                <b><?php echo $service['name']; ?></b>
-                                                <?php echo $service['description']; ?>
-                                                <?php if($service['many']): ?>
-                                                    <input type="text" class="WSC_serviceCount" size="3" value="1" />
+                                <div class="WSC_sectionCategoryServices">
+                                    <?php foreach (Service::getByCategory($category['id']) as $service): ?>
+                                        <div class="WSC_service">
+                                            <div class="wrap_name pull-left"><?php echo $service['name']; ?></div>
+                                            <div class="wrap_available pull-left">
+                                                <?php if ($service['many']): ?>
                                                     Доступно: <?php echo $service['maxCount']; ?>
                                                 <?php endif; ?>
-                                                <div class="WSC_serviceCostContainer">
-                                                    Цена: <?php echo $service['cost']; ?> руб.
-                                                </div>
                                             </div>
-                                        <?php endforeach; ?>
-                                    </div>
+                                            <div class="WSC_serviceCostContainer wrap_cost pull-right">
+                                                <?php echo $service['cost']; ?><i class="fa fa-rub"></i>
+                                            </div>
 
+                                            <div class="wrap_checkbox pull-right">
+                                                <input type="checkbox" class="WSC_serviceCheck"/>
+                                                <input type="hidden" class="WSC_serviceCostValue" value="<?php echo $service['cost']; ?>"/>
+                                                <input type="hidden" class="WSC_serviceManyValue" value="<?php echo $service['many']; ?>"/>
+                                            </div>
+
+                                            <div class="wrap_many pull-right">
+                                                <?php if ($service['many']): ?>
+                                                    <input type="text" class="WSC_serviceCount" size="3" value="1"/>
+                                                <?php endif; ?>
+                                            </div>
+
+                                            <div class="wrap_desc">
+                                                <?php echo $service['description']; ?>
+                                            </div>
+
+                                        </div>
+                                    <?php endforeach; ?>
                                 </div>
-                            <?php endforeach; ?>
 
-                        </div>
-                    <?php endforeach; ?>
-                </div>
+                            </div>
+                        <?php endforeach; ?>
+
+                    </div>
+                <?php endforeach; ?>
             </div>
-        <?php
+        </div>
+    <?php
     }
 }
